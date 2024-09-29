c958d145861f5928b8af2ddba13b6a3e45d0e884
AlonCohen96 <aloncohen36@gmail.com>
2024-09-29 13:36:31 +0200

    inserted placeholder gitbooks

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,44 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      hello
-        hello
-        hello
-    </>
+      <div id='site-container'>
+        <h2>Projects</h2>
+        <ul>
+          <li data-id='web-annotation-interface'>
+            <div>
+              <p>Annotation Web Interface</p>
+              <button>
+                ➤
+              </button>
+            </div>
+            <iframe
+                id='web-annotation-interface'
+                src='https://nccr-liri.gitbook.io/annotation-web-interface-docs/'
+                width='100%'
+                height='1000px'
+                frameBorder='0'
+                allowFullScreen
+            ></iframe>
+          </li>
+
+          <li data-id='another-project'>
+            <div>
+              <p>Another Project</p>
+              <button>
+                ➤
+              </button>
+            </div>
+            <iframe
+                id='another-project'
+                src='https://nccr-liri.gitbook.io/annotation-web-interface-docs/'
+                width='100%'
+                height='1000px'
+                frameBorder='0'
+                allowFullScreen
+            ></iframe>
+          </li>
+        </ul>
+      </div>
   )
 }
 
