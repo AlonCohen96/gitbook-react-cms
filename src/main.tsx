a9942478f97860b0e1b1d63615ef29fc8d73bfba
AlonCohen96 <aloncohen36@gmail.com>
2024-09-29 17:32:22 +0200

    installed react-to-webcomponent

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client'
+import reactToWebComponent from 'react-to-webcomponent';
 import App from './App.tsx'
 import './index.css'
+
+// Convert the React component to a Web Component
+const AppWebComponent = reactToWebComponent(App, React, ReactDOM);
+
+// Define the new custom element
+customElements.define('app-web-component', AppWebComponent);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
