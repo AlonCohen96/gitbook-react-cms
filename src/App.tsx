81bd28ba1961145aa9366c3fa4d4a8730d9cffcd
AlonCohen96 <aloncohen36@gmail.com>
2024-09-29 14:43:45 +0200

    dynamically close and open gitbooks

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,50 @@ function App() {
   const [projects, setProjects] = useState([
       {
         name: 'Annotation Web Interface',
-        link: 'https://nccr-liri.gitbook.io/annotation-web-interface-docs/'
+        link: 'https://nccr-liri.gitbook.io/annotation-web-interface-docs/',
+        id: nanoid(),
+        visible: false,
+      },
+      {
+          name: 'Project 2',
+          link: 'https://nccr-liri.gitbook.io/annotation-web-interface-docs/',
+          id: nanoid(),
+          visible: false,
+      },
+      {
+          name: 'Project 3',
+          link: 'https://nccr-liri.gitbook.io/annotation-web-interface-docs/',
+          id: nanoid(),
+          visible: false,
       }
   ])
+
+    const toggleGitbook = (projectId) => {
+          const updatedProjects = projects.map(project => {
+              if (projectId === project.id){
+                  return {...project, visible: !project.visible}
+              }
+              return project
+          })
+        setProjects(updatedProjects)
+    }
 
   return (
       <>
         {projects.map(project =>
-          <div key={nanoid()}>
+          <div key={project.id}>
             {project.name}
-            <iframe
-                src={project.link}
-                width='100%'
-                height='1000px'
-                allowFullScreen
-            >
-            </iframe>
-            <button onClick={() => setProjects([])}>
-              don't click me yet
+              {project.visible &&
+                  <iframe
+                      src={project.link}
+                      width='100%'
+                      height='500px'
+                      allowFullScreen
+                  >
+                  </iframe>
+              }
+            <button onClick={() => toggleGitbook(project.id)}>
+              Show/Hide Gitbook
             </button>
           </div>
         )}
