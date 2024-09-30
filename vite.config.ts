21bae4a737292a6a3793f809edc03a2021c63260
AlonCohen96 <aloncohen36@gmail.com>
2024-09-30 16:11:56 +0200

    updated vite.config.ts to always use the same index.js file name

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',  // Fixed name for the main entry file
+        chunkFileNames: 'index.js',   // Fixed name for chunks (if applicable)
+        assetFileNames: 'index.js',    // Fixed name for assets
+      },
+    },
+  },
 })
