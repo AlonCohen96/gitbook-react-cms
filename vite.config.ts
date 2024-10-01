b1281b427e7c9434d13c82973ee34f2cf73e79f8
AlonCohen96 <aloncohen36@gmail.com>
2024-10-01 15:07:11 +0200

    updated vite config to include css files

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist/assets',
+    cssCodeSplit: true,  // Enable CSS code splitting
     rollupOptions: {
       output: {
-        entryFileNames: 'index.js',  // Fixed name for the main entry file
-        chunkFileNames: 'index.js',   // Fixed name for chunks (if applicable)
-        assetFileNames: 'index.js',    // Fixed name for assets
+        entryFileNames: 'index.js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',  // Use for both CSS and other assets
       },
     },
   },
-})
+});
